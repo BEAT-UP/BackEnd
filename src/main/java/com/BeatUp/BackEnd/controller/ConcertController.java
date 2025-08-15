fcb7e470c31386bfe50bfa177271f147808ada9b
junhui <junhuilim@kakao.com>
2025-08-15 17:42:59 +0900

    [FEAT] Concert API 구현

---
+++ src/main/java/com/BeatUp/BackEnd/controller/ConcertController.java
@@ -0,0 +1,44 @@
+package com.BeatUp.BackEnd.controller;
+
+
+import com.BeatUp.BackEnd.entity.Concert;
+import com.BeatUp.BackEnd.service.ConcertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/concert")
+public class ConcertController {
+
+    @Autowired
+    private ConcertService concertService;
+
+    @GetMapping("/concerts")
+    public Map<String, Object> getConcerts(
+            @RequestParam(required = false) String query,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate date){
+
+        List<Concert> concerts = concertService.getConcerts(query, date);
+
+        return Map.of(
+                "concerts", concerts,
+                "total", concerts.size(),
+                "query", query != null ? query: "",
+                "date", date != null ? date.toString() : ""
+        );
+    }
+
+    @GetMapping("/concerts/{id}")
+    public ResponseEntity<Concert> getConcertById(@PathVariable UUID id){
+        return concertService.getConcertById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/com/BeatUp/BackEnd/service/ConcertService.java
@@ -0,0 +1,42 @@
+package com.BeatUp.BackEnd.service;
+
+
+import com.BeatUp.BackEnd.entity.Concert;
+import com.BeatUp.BackEnd.repository.ConcertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ConcertService {
+
+    @Autowired
+    private ConcertRepository concertRepository;
+
+    // 공연 목록 조회(검색 + 날짜 필터)
+    public List<Concert> getConcerts(String query, LocalDate date){
+        LocalDateTime startOfDay = null;
+        LocalDateTime endOfDay = null;
+
+        // LocalDate를 LocalDateTime 범위로 변환
+        if(date != null)
+        {
+            startOfDay = date.atStartOfDay(); // 해당 날짜의 00:00:00
+            endOfDay = date.atTime(LocalTime.MAX); // 해당 날짜의 23:59:59.9999
+        }
+
+        return concertRepository.findByQueryAndDate(query, startOfDay, endOfDay);
+    }
+
+    // 공연 상세 조회
+    public Optional<Concert> getConcertById(UUID id){
+        return concertRepository.findById(id);
+    }
+}
