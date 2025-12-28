3c7a5bdec04b073bdee81b05148782d5dce9ec9c
junhui <junhuilim@kakao.com>
2025-12-28 21:16:22 +0900

    [fix]: test 관련 애너테이션 수정

--- src/test/java/com/BeatUp/BackEnd/common/config/TestFirebaseConfig.java
+++ src/test/java/com/BeatUp/BackEnd/common/config/TestFirebaseConfig.java
@@ -1,8 +1,9 @@
 package com.BeatUp.BackEnd.common.config;
 
 import com.google.firebase.messaging.FirebaseMessaging;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
@@ -11,12 +12,13 @@ import static org.mockito.Mockito.mock;
 /**
  * 테스트 환경에서 FirebaseMessaging을 Mock으로 제공
  */
-@TestConfiguration
+@Configuration
 @Profile("test")
 public class TestFirebaseConfig {
 
     @Bean
     @Primary
+    @ConditionalOnMissingBean(FirebaseMessaging.class)
     public FirebaseMessaging firebaseMessaging() {
         return mock(FirebaseMessaging.class);
     }
